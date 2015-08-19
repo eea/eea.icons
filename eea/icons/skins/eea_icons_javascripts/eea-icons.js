31718de93fed5f1920d32e7b22b47a35ca18b780
ichim-david <ichim.david@gmail.com>
2015-08-19 15:59:51 +0300

    [ichimdav #27215] Added missing commit added already in custom where eea-icons.js now checks if we need to add the eea-icon span within the eea-icon-containers

--- eea/icons/skins/eea_icons_javascripts/eea-icons.js
+++ eea/icons/skins/eea_icons_javascripts/eea-icons.js
@@ -1,13 +1,21 @@
 jQuery(document).ready(function ($) {
 
   $(".eea-icon-left-container").each(function (i, el) {
+    var $el = $(el);
+    if ($el.find(".eea-icon-left").length) {
+      return;
+    }
     var $i_tag = $("<span class='eea-icon eea-icon-left'></span>");
-    $(el).prepend($i_tag);
+    $el.prepend($i_tag);
   });
 
   $(".eea-icon-right-container").each(function (i, el) {
+    var $el = $(el);
+    if ($el.find(".eea-icon-right").length) {
+      return;
+    }
     var $i_tag = $("<span class='eea-icon eea-icon-right'></span>");
-    $(el).append($i_tag);
+    $el.append($i_tag);
   });
 
 });
